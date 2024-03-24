5a92fdfb49e55149a8accb2da7a198bbf67c2bc8
jeremy.lim <jeremy.lim@unimelb.edu.au>
2024-03-25 10:23:34 +1100

    GVA Writeup and documentation
    Works, just need to check it converges. AEL function adjusted to work for GVA and using array vs list.

--- R/compute-AEL-functions.R
+++ R/compute-AEL-functions.R
@@ -1,92 +1,94 @@
 #' Computes Accurate Empirical Likelihood Interference for a data set using R
-#' 
-#' @description 
-#' `compute_AEL` returns the result of running AEL with the values present in 
+#'
+#' @description
+#' `compute_AEL` returns the result of running AEL with the values present in
 #' the arguments.
-#' 
-#' @param th    Vector or scalar
-#' @param h     User-defined function, outputs array
-#' @param lam0  Initial vector for lambda
-#' @param a     Scalar constant
-#' @param z     n-1 by d matrix
-#' @param T     Number of iterations using Newton-Raphson for estimation of lambda (default: 100)
+#'
+#' @param th        Vector or scalar theta, kx1 matrix
+#' @param h         User-defined moment-condition function, outputs a kx1 matrix containing the kth row of h. Function must take two arguments: zi and theta for h(zi,th)
+#' @param lam0      Initial guess for lambda, kx1 matrix
+#' @param a         Scalar AEL constant, must be >0, small values recommended
+#' @param z         n-1 by d data matrix
+#' @param T         Number of iterations using Newton-Raphson for estimation of lambda (default: 500)
+#' @param returnH   Whether to return calculated values of h, H matrix and lambda
 #'
 #' @return The AEL of the data set
 #' @export
 #'
 #' @examples compute_AEL_R(matrix(c(0.8277, -1.0050), nrow = 2), function(z, th) {matrix(c(z[2] - th[1] - th[2] * z[1], z[1]*(z[2] - th[1] - th[2] * z[1])), nrow = 2)}, matrix(c(0,0), nrow = 2), 0.001, cbind(runif(30, min = -5, max = 5), 0.75 - runif(30, min = -5, max = 5) + rnorm(30, mean = 0, sd = 1)))
 
-compute_AEL_R <- function(th, h, lam0, a, z, T) {
-
-  # -----------------------------
-  # Default values
-  # -----------------------------
-  if (missing(T)) {
-    T <- 100
-  }
-  
-  # -----------------------------
-  # Starting variables (h(zi,th), h(zn,th), H)
-  # -----------------------------
-  h_list <- list() # Initialise
-  h_sum <- 0
-  H_Zth <- c()
-  
-  n <- nrow(z) + 1
-  
-  for (i in 1:(n - 1)) {
-    zi <- t(z[i,]) # Row of z as vertical vector
-    h_zith <- h(zi,th)
+compute_AEL_R <- function(th, h, lam0, a, z, T, returnH) {
+    # -----------------------------
+    # Default values
+    # -----------------------------
+    if (missing(T)) { T <- 500 }
+    if (missing(returnH)) { returnH <- 0 }
+    # -----------------------------
+    # Starting variables (h(zi,th), h(zn,th), H)
+    # -----------------------------
+    n <- nrow(z) + 1
+    h_arr <- array(dim = c(dim(h(t(z[1, ]),th)),n)) # Initialise
+    h_sum <- 0
+    H_Zth <- c()
     
-    h_list[[i]] <- h_zith # Collect all h(zi,th) so no need to calculate again
+    for (i in 1:(n - 1)) {
+        zi <- t(z[i, ]) # Row of z as vertical vector
+        h_zith <- h(zi, th)
+        
+        h_arr[,,i] <-
+            h_zith # Collect all h(zi,th) so no need to calculate again
+        
+        h_sum <- h_sum + h_zith # For h(zn,th)
+        H_Zth <- rbind(H_Zth, t(h_zith)) # Build up H(Z,th)
+    }
     
-    h_sum <- h_sum + h_zith # For h(zn,th)
-    H_Zth <- rbind(H_Zth, t(h_zith)) # Build up H(Z,th)
-  }
-  
-  h_znth <- -a / (n - 1) * h_sum
-  H_Zth <- rbind(H_Zth, t(h_znth)) # Last row of H is h(zn,th)
-  
-  h_list[[n]] <- h_znth
-  
-  # -----------------------------
-  # Lambda Calculation
-  # -----------------------------
-  lambda <- compute_lambda(h_list, H_Zth, lam0, a, T, n)
+    h_znth <- -a / (n - 1) * h_sum
+    H_Zth <- rbind(H_Zth, t(h_znth)) # Last row of H is h(zn,th)
+    
+    h_arr[,,n] <- h_znth
+    
+    # -----------------------------
+    # Lambda Calculation
+    # -----------------------------
+    lambda <- compute_lambda(h_arr, H_Zth, lam0, a, T, n)
 
-  # -----------------------------
-  # AEL Calculation
-  # -----------------------------
-  temp_accu <- 0
-  for (i in 1:n) {
-    temp_accu <- temp_accu + log(1 + t(lambda) %*% h_list[[i]])
-  }
-  log_AEL <- -(temp_accu + n * log(n))
-  
-  log_AEL[1,1]
+    # -----------------------------
+    # AEL Calculation
+    # -----------------------------
+    temp_accu <- 0
+    for (i in 1:n) {
+        temp_accu <- temp_accu + log(1 + t(lambda) %*% h_arr[,,i])
+    }
+    log_AEL <- -(temp_accu + n * log(n))
+
+    if (!returnH) {
+        log_AEL
+    } else {
+        return(list("log_AEL" = log_AEL[1, 1], "lambda" = lambda, "h_arr" = h_arr, "H" = H_Zth))
+    }
 }
 
-compute_lambda <- function(h_list, H_Zth, lam0, a, T, n) {
+compute_lambda <- function(h_arr, H_Zth, lam0, a, T, n) {
     #' Computes lambda<sup>T</sup> for AEL for a specific set of user-inputted variables
-    #' 
-    #' @description 
-    #' `compute_lambda` returns the result of running a modified Newton-Raphson to 
+    #'
+    #' @description
+    #' `compute_lambda` returns the result of running a modified Newton-Raphson to
     #' find the lambda variable needed in calculating AEL
-    #' 
-    #' @details 
+    #'
+    #' @details
     #' juicy deets
-    #' 
-    #' @param h_list  List of h(z1,th) to h(zn,th)
+    #'
+    #' @param h_arr  List of h(z1,th) to h(zn,th)
     #' @param H_Zth   Hessian matrix H
     #' @param lam0    Initial vector for lambda
     #' @param a       Scalar constant
     #' @param T       Number of iterations using Newton-Raphson for estimation of lambda
     #' @param n       Height/Width of z + 1
-    #' 
+    #'
     #' @keywords internal
     #' @noRd
     
-    ## Functions  
+    ## Functions
     
     get_wi_arr <- function(tild_lam) {
         #' @param tild_lam  Vector, current iteration of lambda
@@ -94,7 +96,8 @@ compute_lambda <- function(h_list, H_Zth, lam0, a, T, n) {
         wi_arr <- c() # Store for use in D in P
         for (i in 1:n) {
             # store as vector so can use in multiplication later, if not it fails
-            wi_arr[i] <- as.vector((1 + t(tild_lam) %*% h_list[[i]]) ^-1) 
+            wi_arr[i] <-
+                as.vector((1 + t(tild_lam) %*% h_arr[,,i]) ^ -1)
         }
         
         wi_arr
@@ -112,11 +115,11 @@ compute_lambda <- function(h_list, H_Zth, lam0, a, T, n) {
             if (wi^-1 >= 1 / n) {
                 vi <- wi
             } else {
-                vi <- 2 * n - n^2 / wi
+                vi <- 2 * n - n ^ 2 / wi
             }
             
             # Calculate sum for this iteration of dF
-            dF <- dF + vi * h_list[[i]]
+            dF <- dF + vi * h_arr[,,i]
         }
         
         dF
@@ -129,16 +132,16 @@ compute_lambda <- function(h_list, H_Zth, lam0, a, T, n) {
         #' @param wi_arr    Array of vectors of wi(th, lam~)
         
         # Build diagonal matrix
-        # Diagonal matrix changes with respect to lambda's current guess so need 
+        # Diagonal matrix changes with respect to lambda's current guess so need
         # to recompute each iteration
         D_arr <- c()
         for (i in 1:n) {
-            if (wi_arr[i]^-1 >= 1/n) {
+            if (wi_arr[i]^-1 >= 1 / n) {
                 vi2 <- wi_arr[i]
             } else {
                 vi2 <- n
             }
-            D_arr[i] <- vi2^2
+            D_arr[i] <- vi2 ^ 2
         }
         D <- diag(D_arr)
         
@@ -164,6 +167,4 @@ compute_lambda <- function(h_list, H_Zth, lam0, a, T, n) {
         lam_prev <- lam_prev - solve(P) %*% dF
     }
     lambdaT <- lam_prev # Final lambda
-    
-    lambdaT
 }  
---
+++ R/compute-GVA-functions.R
@@ -0,0 +1,84 @@
+#' Computes Full-Covariance Gaussian VB posterior in R
+#'
+#' @param mu            Column vector, initial value of Gaussian VB mean
+#' @param C             Lower Triangular Matrix, initial value of Gaussian VB Cholesky
+#' @param h             User-defined moment-condition function, outputs a kx1 matrix containing the kth row of h. Function must take two arguments: zi and theta for h(zi,th)
+#' @param delthh        User defined function, outputs kxp Jacobian matrix of h(zi,th) with respect to theta
+#' @param delth_logpi   User-defined function, outputs px1 matrix, derivative of log prior function
+#' @param z             n-1 by d data matrix
+#' @param rho           Scalar (0 <~ 1) ADADELTA accumulation constant
+#' @param elip          Scalar numeric stability constant. Should be specified with a small value
+#' @param T             number of iterations for GVA (default:10,000)
+#' @param T2            number of iterations for Log AEL (default:500)
+#' @param th            Vector or scalar theta, kx1 matrix
+#' @param lam0          Initial guess for lambda, kx1 matrix
+#' @param a             Scalar AEL constant, must be >0, small values recommended
+
+#'
+#' @returns             List of mu_FC and C_FC. Access using those names.
+#' @export
+#'
+#' @examples
+compute_GVA_R <- function(mu, C, h, delthh, delth_logpi, z, rho, elip, T, T2, theta, lam0, a) {
+    # Initialise values
+    if (missing(T)) { T <- 10000 }
+    if (missing(T2)) { T2 <- 500 }
+    
+    p           <- nrow(C)
+    Egmu        <- numeric(p)
+    Edelmu      <- numeric(p)
+    EgC         <- matrix(0, nrow = p, ncol = p)
+    EdelC       <- matrix(0, nrow = p, ncol = p)
+    mu_t        <- mu
+    mu_arr      <- array(dim = c(dim(mu_t), T+1))
+    mu_arr[,,1] <- mu_t
+    C_t         <- C        # Covariance Cholesky
+    C_arr       <- array(dim = c(dim(C_t), T+1))
+    C_arr[,,1]  <- C_t
+    M           <- matrix(1,p,p)
+    n           <- nrow(z) + 1
+
+    xi          <- matrix(rnorm(T*p),T,p)                   # I     - Draw xi
+    
+    for (i in 1:(T)) {
+        th      <- mu_t + C_t %*% xi[i,]                    # II    - Set theta
+        gmu     <- compute_nabmu_ELBO(delth_logpi, delthh, 
+                                      th, n, h, lam0, 
+                                      a, z, T2)             # III   - Compute g_{mu}^{t+1}
+        Egmu    <- rho * Egmu + (1 - rho) * gmu^2           # IV    - Accumulate gradients
+        delmu   <- sqrt(Edelmu + elip * rep(1,p)) / 
+            sqrt(Egmu + elip * rep(1,p)) * gmu              # V     - Compute update
+        mu_t    <- mu_t + delmu                             # VI    - Update mean
+        Edelmu  <- rho * Edelmu + (1 - rho) * delmu^2       # VII   - Accumulate updates
+        gC_t    <- compute_nabC_ELBO(gmu, xi[i,], C_t)      # VIII  - Compute g_C^{t+1}
+        gC_t[upper.tri(gC_t)] <- 0                          #       - Set gC_t to lower triag matx
+        EgC     <- rho * EgC + (1 - rho) * gC_t^2           # IX    - Accumulate gradients
+        delC    <- sqrt(EdelC + elip * M) / 
+            sqrt(EgC + elip * M) * gC_t                     # X     - Compute update
+        C_t     <- C_t + delC                               # XI    - Update covariance Cholesky
+        EgC     <- rho * EgC + (1 - rho) * delC^2           # XII   - Accumulate updates
+        # browser()
+        mu_arr[,,i+1]   <- mu_t
+        C_arr[,,i+1]    <- C_t
+    }
+
+    return(list("mu_FC" = mu_t, "C_FC" = C_t, "mu_FC_arr" = mu_arr, "C_FC_arr" = C_arr))
+}
+
+compute_nabmu_ELBO <- function(delth_logpi, delthh, theta, n, h, lam0, a, z, T2) { 
+    res <- compute_AEL_R(theta, h, lam0, a, z, T2, 1) # list("log_AEL" = log_AEL[1, 1], "lambda" = lambda, "h_arr" = h_arr, "H" = H_Zth)
+    lambda <- res$"lambda"
+    h_arr <- res$"h_arr"
+    hznth <- h_arr[,,n]
+
+    # Calculate gradient LogAEL with respect to theta
+    nabth_logAEL <- 0 # Matrix
+    for (i in 1:(n-1)) {
+        nabth_logAEL <- nabth_logAEL - (1/(1 + t(lambda) %*% h_arr[,,i]) - (a/(n-1)) / (1 + t(lambda) %*% hznth))[1] * (t(delthh(z[i], theta)) %*% lambda)
+    }
+    nabmu_ELBO <- nabth_logAEL + delth_logpi(theta)
+}
+
+compute_nabC_ELBO <- function(gmu, xi, C_t) {
+    nabC_ELBO <- gmu %*% t(xi) + diag(1/diag(C_t))
+}
